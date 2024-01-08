3b5c9b6ec6290ed4043cb2a7b6be1a742b6299c8
Stasia <stasia@Stasias-MacBook.local>
2024-01-08 14:35:15 +0200

    init project + setup eslint + dependencies

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Intro } from 'components';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="app">
+      <Intro />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Intro/Intro.jsx
@@ -0,0 +1,68 @@
+import React, { useState } from 'react';
+import './Intro.scss';
+import { useGSAP } from '@gsap/react';
+import gsap from 'gsap';
+
+const initialPhrase = 'вітаю, я Давід. це моє тестове завдання для StarNavi ☺';
+const colors = [
+  '#D9EDBF',
+  '#FDFFAB',
+  '#FFB996',
+  '#DCF2F1',
+  '#F3CCF3',
+  '#A1EEBD',
+  '#DED0B6',
+  '#C1F2B0',
+  '#CD8D7A',
+];
+
+export const Intro = ({ phrase = initialPhrase }) => {
+  useGSAP(() => {
+    const words = gsap.utils.toArray('p');
+
+    words.forEach((word, index) => {
+      gsap.set(word, {
+        backgroundColor: colors[index],
+        rotation: gsap.utils.random(-15, 15),
+      });
+    });
+
+    gsap.from('p', {
+      opacity: 0,
+      duration: 0.3,
+      stagger: 0.3,
+      ease: 'power2.inOut',
+      onComplete: () => {
+        gsap.from('p:last-child', {
+          rotation: 360,
+          opacity: 0,
+          repeat: -1,
+          repeatDelay: 2,
+          duration: 0.6,
+          ease: 'circ.out',
+        });
+      },
+    });
+  });
+
+  return (
+    <div className="intro">
+      {phrase.split(' ').map((word) => (
+        <Word key={word} word={word} />
+      ))}
+    </div>
+  );
+};
+
+const Word = ({ word }) => {
+  const [isVisible, setIsVisible] = useState(true);
+  return (
+    <>
+      {isVisible && (
+        <p onMouseEnter={() => setIsVisible(false)} className="intro__word">
+          {word}
+        </p>
+      )}
+    </>
+  );
+};
